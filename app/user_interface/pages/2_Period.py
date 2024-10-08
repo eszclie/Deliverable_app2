92c6273a3d1883f18a78968ccf07b857cd7768be
eszclie <eszclie@gmail.com>
2024-10-08 10:32:39 +0200

    Update 2_Period.py

--- app/user_interface/pages/2_Period.py
+++ app/user_interface/pages/2_Period.py
@@ -4,8 +4,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-import src.etl_neerslag.functions.get_average as avg
-from app.user_interface.funcs import bind_socket, get_response, set_page_confic
+import get_average as avg
+from funcs import bind_socket, get_response, set_page_confic
 
 # Set the name and icon of the page
 set_page_confic()
