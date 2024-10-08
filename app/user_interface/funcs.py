cca263af226743e3ef7e5b8482f3735ea3b7f57f
eszclie <eszclie@gmail.com>
2024-10-08 10:41:55 +0200

    Update funcs.py

--- app/user_interface/funcs.py
+++ app/user_interface/funcs.py
@@ -30,10 +30,12 @@ def bind_socket():
     endpoint_name = "endpoint-xgb-model"
 
     # Get the endpoint credentials
-    endpoint = ml_client.online_endpoints.get(endpoint_name)
-    scoring_uri = endpoint.scoring_uri
-    keys = ml_client.online_endpoints.get_keys(name=endpoint_name)
-    key = keys.primary_key
+    # endpoint = ml_client.online_endpoints.get(endpoint_name)
+    # scoring_uri = endpoint.scoring_uri
+    # keys = ml_client.online_endpoints.get_keys(name=endpoint_name)
+    # key = keys.primary_key
+    scoring_uri = os.environ["key"]
+    key = os.environ["score"]
     headers = {"Authorization": ("Bearer " + key)}
     return scoring_uri, headers
 
